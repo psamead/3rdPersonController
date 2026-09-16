63cd80db031eaef1c28550d5729a7293b73a994f
Haoxi Yang <yhxbarking@hotmail.com>
2026-09-17 06:20:32 +0800

    Add player locomotion and management scripts, along with URP settings
    - Implemented PlayerLocomotion.cs for handling player movement and rotation based on camera direction.
    - Created PlayerManager.cs to manage player inputs and locomotion updates.
    - Added DefaultVolumeProfile.asset and UniversalRenderPipelineGlobalSettings.asset for URP configuration.
    - Included corresponding .meta files for all new assets.

--- Assets/Script/InputManager.cs
+++ Assets/Script/InputManager.cs
@@ -7,6 +7,8 @@ public class InputManager : MonoBehaviour
     PlayerControls playerControls;
 
     public Vector2 movementInput;
+    public float verticalInput;
+    public float horizontalInput;
 
     private void OnEnable()
     {
@@ -24,5 +26,20 @@ public class InputManager : MonoBehaviour
     private void OnDisable()
     {
         playerControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
+        // HandleJumpingInput
+        // HandleActionInput
+    }
+
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+
     }
 }
